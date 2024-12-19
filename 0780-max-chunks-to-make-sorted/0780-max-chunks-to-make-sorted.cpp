fca19415bda2ae14770b5ffc89f9893e54936617
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2024-12-19 15:26:03 +0530

    Time: 0 ms (100%), Space: 9.4 MB (7.32%) - LeetHub

---
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        const int n=arr.size();
+        int count=0, diff=0;
+        for(int i=0; i<n; i++){
+            diff+=arr[i]-i;
+            count+=(diff==0);
+        }
+        return count;
+    }
+};
