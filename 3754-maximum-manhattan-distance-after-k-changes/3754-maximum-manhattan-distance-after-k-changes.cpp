6730d89ce9734f42b3c5f21626e102f37fede155
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2025-06-21 17:58:11 +0530

    Time: 143 ms (50.14%), Space: 38.1 MB (89.86%) - LeetHub

---
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    int solve(char a, char b, string& s, int k){
+        int dist=0, mx=0;
+
+        for(int i=0;i<s.size();i++){
+            if(a==s[i] or b==s[i]){dist++;}
+            else{
+                if(k>0){k--; dist++;}
+                else{
+                    mx = max(mx, dist);
+                    dist--;
+                }
+            }
+        }
+        mx = max(mx, dist);
+
+        return mx;
+    }
+public:
+    int maxDistance(string s, int k) {
+        if(k==s.size()){return k;}
+
+        int nw = solve('N','W',s, k);
+        int ne = solve('N','E',s, k);
+        int sw = solve('S','W',s, k);
+        int se = solve('S','E',s, k);
+
+        return max({nw, ne, sw, se});
+    }
+};
