f2a00a6d128f76cf92ac062938ea4d3b53e459fe
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2025-08-21 21:03:26 +0530

    Time: 36 ms (21.18%), Space: 15.6 MB (100%) - LeetHub

--- 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
@@ -1,38 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int numSubmat(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> newmat(n, vector<int>(m, 0));
-
-        for (int i = 0; i < n; i++) {
-            newmat[i][m-1] = mat[i][m-1] == 0 ? 0 : 1;
-            for (int j = m-2; j >= 0; j--) {
-                newmat[i][j] = mat[i][j] == 0 ? 0 : newmat[i][j+1] + 1;
-            }
-        }
-
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int minwidth = newmat[i][j];
-                for (int d = i; d < n; d++) {
-                    if (newmat[d][j] == 0) break;
-                    minwidth = min(minwidth, newmat[d][j]);
-                    count += minwidth;
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> g(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] == 1) {
+                    g[i][j] = j == 0 ? 1 : 1 + g[i][j - 1];
                 }
             }
         }
-        return count;
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int col = 1 << 30;
+                for (int k = i; k >= 0 && col > 0; --k) {
+                    col = min(col, g[k][j]);
+                    ans += col;
+                }
+            }
+        }
+        return ans;
     }
 };
-
-int main() {
-    Solution sol;
-    vector<vector<int>> mat = {{1,0,1},{1,1,0},{1,1,0}};
-    cout << sol.numSubmat(mat) << endl;
-    return 0;
-}
