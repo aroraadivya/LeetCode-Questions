3e543cdeda029ff5f4fbb06d9e4891b45974c170
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2026-04-27 23:17:40 +0530

    Time: 198 ms (58.21%), Space: 22.9 MB (100%) - LeetHub

---
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,35 @@
+# Python 3
+from collections import deque
+from typing import List
+
+class Solution:
+    def hasValidPath(self, grid: List[List[int]]) -> bool:
+        m, n = len(grid), len(grid[0])
+        
+        connects = {
+            1: {0, 1},
+            2: {2, 3},
+            3: {0, 3},
+            4: {1, 3},
+            5: {0, 2},
+            6: {1, 2},
+        }
+        dr = [0, 0, -1, 1]
+        dc = [-1, 1, 0, 0]
+        opp = [1, 0, 3, 2]
+        
+        visited = [[False] * n for _ in range(m)]
+        queue = deque([(0, 0)])
+        visited[0][0] = True
+        
+        while queue:
+            r, c = queue.popleft()
+            if r == m - 1 and c == n - 1:
+                return True
+            for d in range(4):
+                nr, nc = r + dr[d], c + dc[d]
+                if 0 <= nr < m and 0 <= nc < n and not visited[nr][nc]:
+                    if d in connects[grid[r][c]] and opp[d] in connects[grid[nr][nc]]:
+                        visited[nr][nc] = True
+                        queue.append((nr, nc))
+        return False
